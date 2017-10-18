f5acfa6ddb0f87a1d7d9351f998f457c322215be
Yuyang Yu <yuyang.yu105@gmail.com>
2017-10-17 23:58:55 -0700

    Update and rename CIFAR-10.py to CIFAR-10_ResNet.py
    MXNet-Gluon implementation of ResNet on CIFAR-10

--- CIFAR-10_ResNet.py
+++ CIFAR-10_ResNet.py
@@ -1,64 +1,10 @@
 
 # coding: utf-8
 
-# # 实战Kaggle比赛——使用Gluon对原始图像文件分类（CIFAR-10）
-# 
-# 我们在[监督学习中的一章](../chapter_supervised-learning/kaggle-gluon-kfold.md)里，以[房价预测问题](https://www.kaggle.com/c/house-prices-advanced-regression-techniques)为例，介绍了如何使用``Gluon``来实战[Kaggle比赛](https://www.kaggle.com)。
-# 
-# 我们在本章中选择了Kaggle中著名的[CIFAR-10原始图像分类问题](https://www.kaggle.com/c/cifar-10)。我们以该问题为例，为大家提供使用`Gluon`对原始图像文件进行分类的示例代码。
-# 
-# 计算机视觉一直是深度学习的主战场，请
-# 
-# > Get your hands dirty。
-# 
-# 
-# 
-# 
-# ## Kaggle中的CIFAR-10原始图像分类问题
-# 
-# [Kaggle](https://www.kaggle.com)是一个著名的供机器学习爱好者交流的平台。为了便于提交结果，请大家注册[Kaggle](https://www.kaggle.com)账号。然后请大家先点击[CIFAR-10原始图像分类问题](https://www.kaggle.com/c/cifar-10)了解有关本次比赛的信息。
-# 
-# ![](../img/kaggle_cifar10.png)
-# 
-# 
-# 
-# ## 整理原始数据集
-# 
-# 比赛数据分为训练数据集和测试数据集。训练集包含5万张图片。测试集包含30万张图片：其中有1万张图片用来计分，但为了防止人工标注测试集，里面另加了29万张不计分的图片。
-# 
-# 两个数据集都是png彩色图片，大小为$32\times 32 \times 3$。训练集一共有10类图片，分别为飞机、汽车、鸟、猫、鹿、狗、青蛙、马、船和卡车。
-# 
-# （那么问题来了，你觉得你用肉眼能把下面100个图片正确分类吗？）
-# 
-# ![](../img/cifar10.png)
-# 
-# 
-# ### 下载数据集
-# 
-# 
-# 登录Kaggle后，数据可以从[CIFAR-10原始图像分类问题](https://www.kaggle.com/c/cifar-10)中下载。
-# 
-# * [训练数据集train.7z下载地址](https://www.kaggle.com/c/cifar-10/download/train.7z)
-# 
-# * [测试数据集test.7z下载地址](https://www.kaggle.com/c/cifar-10/download/test.7z)
-# 
-# * [训练数据标签trainLabels.csv下载地址](https://www.kaggle.com/c/cifar-10/download/trainLabels.csv)
-# 
-# 
-# ### 解压数据集
-# 
-# 训练数据集train.7z和测试数据集test.7z都是压缩格式，下载后请解压缩。解压缩后原始数据集的路径可以如下：
-# 
-# * ../data/kaggle_cifar10/train/[1-50000].png
-# * ../data/kaggle_cifar10/test/[1-300000].png
-# * ../data/kaggle_cifar10/test/trainLabels.csv
-# 
-# 为了使网页编译快一点，我们在gitrepo里仅仅存放100个训练样本（'train_tiny.zip'）和1个测试样本（'test_tiny.zip'）。执行以下代码会从git repo里解压生成小样本训练和测试数据，文件夹名称分别为'train_tiny'和'test_tiny'。训练数据标签的压缩文件将被解压成trainLabels.csv。
-
-# In[1]:
+# ResNet Model on CIFAR-10
 
 
-# 如果训练下载的Kaggle的完整数据集，把下面改False
+# Demo for test
 demo = False
 if demo:
     import zipfile
@@ -67,13 +13,12 @@ if demo:
             zin.extractall('../data/kaggle_cifar10/')
 
 
-# ### 整理数据集
+# 整理数据集
 # 
 # 我们定义下面的reorg_cifar10_data函数来整理数据集。整理后，同一类图片将出现在在同一个文件夹下，便于`Gluon`稍后读取。
 # 
 # 函数中的参数如data_dir、train_dir和test_dir对应上述数据存放路径及训练和测试的图片集文件夹名称。参数label_file为训练数据标签的文件名称。参数input_dir时整理后数据集文件夹名称。参数valid_ratio是验证集占原始训练集的比重。以valid_ratio=0.1为例，由于原始训练数据有5万张图片，调参时将有4万5千张图片用于训练（整理后存放在input_dir/train）而另外5千张图片为验证集（整理后存放在input_dir/valid）。
 
-# In[27]:
 
 
 import os
@@ -121,12 +66,9 @@ def reorg_cifar10_data(data_dir, label_file, train_dir, test_dir, input_dir, val
         shutil.copy(os.path.join(data_dir, test_dir, test_file),
                     os.path.join(data_dir, input_dir, 'test', 'unknown'))
 
-
-# 再次强调，为了使网页编译快一点，我们在这里仅仅使用100个训练样本和1个测试样本。训练和测试数据的文件夹名称分别为'train_tiny'和'test_tiny'。相应地，我们仅将批量大小设为1。实际训练和测试时应使用Kaggle的完整数据集。由于数据集较大，批量大小train_batch大小可设为一个较大的整数，例如128。
-# 
+        
 # 我们将10%的训练样本作为调参时的验证集。
 
-# In[28]:
 
 
 if demo:
@@ -152,7 +94,6 @@ reorg_cifar10_data(data_dir, label_file, train_dir, test_dir, input_dir, valid_r
 # 
 # 为避免过拟合，我们在这里使用`image.CreateAugmenter`来加强数据集。例如我们设`rand_mirror=True`即可随机对每张图片做镜面反转。我们也通过`mean`和`std`对彩色图像RGB三个通道分别做[标准化](../chapter_supervised-learning/kaggle-gluon-kfold.md)。以下我们列举了该函数里的所有参数，这些参数都是可以调的。
 
-# In[49]:
 
 
 from mxnet import autograd
@@ -192,7 +133,6 @@ def transform_test(data, label):
 
 # 接下来，我们可以使用`Gluon`中的`ImageFolderDataset`类来读取整理后的数据集。
 
-# In[50]:
 
 
 input_str = data_dir + '/' + input_dir + '/'
@@ -222,36 +162,46 @@ softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
 # 我们这里使用了[ResNet-18](resnet-gluon.md)模型。我们使用[hybridizing](../chapter_gluon-advances/hybridize.md)来提升执行效率。
 # 
 # 请注意：模型可以重新设计，参数也可以重新调整。
-
-# In[51]:
+#
+# New attempt: BN -> Relu -> Conv
 
 
 from mxnet.gluon import nn
 from mxnet import nd
 
 class Residual(nn.HybridBlock):
+    # 参数： channel数，same_shape:因为要做加法，所以shape一不一致很重要
     def __init__(self, channels, same_shape=True, **kwargs):
         super(Residual, self).__init__(**kwargs)
         self.same_shape = same_shape
+        
+        # 存在两个卷积
         with self.name_scope():
+            
             strides = 1 if same_shape else 2
             self.conv1 = nn.Conv2D(channels, kernel_size=3, padding=1,
                                   strides=strides)
             self.bn1 = nn.BatchNorm()
+            
             self.conv2 = nn.Conv2D(channels, kernel_size=3, padding=1)
             self.bn2 = nn.BatchNorm()
+            
+            # 我们需要把shape变成一致，不是same_shape，我们shape被减半了
             if not same_shape:
                 self.conv3 = nn.Conv2D(channels, kernel_size=1,
                                       strides=strides)
 
     def hybrid_forward(self, F, x):
+        # 第一个block
         out = F.relu(self.bn1(self.conv1(x)))
+        # 第二个block
         out = self.bn2(self.conv2(out))
+        # 如果不是same_shape，x做第三层卷积，放回out的形状
         if not self.same_shape:
             x = self.conv3(x)
         return F.relu(out + x)
 
-    
+# ResNet就是n个Residual块相连    
 class ResNet(nn.HybridBlock):
     def __init__(self, num_classes, verbose=False, **kwargs):
         super(ResNet, self).__init__(**kwargs)
@@ -259,10 +209,11 @@ class ResNet(nn.HybridBlock):
         with self.name_scope():
             net = self.net = nn.HybridSequential()
             # block 1
-            net.add(nn.Conv2D(channels=32, kernel_size=3, strides=1, padding=1))
             net.add(nn.BatchNorm())
             net.add(nn.Activation(activation='relu'))
+            net.add(nn.Conv2D(channels=32, kernel_size=3, strides=1, padding=1))
             # block 2
+            #net.add(nn.MaxPool2D(pool_size=3, strides=2))
             for _ in range(3):
                 net.add(Residual(channels=32))
             # block 3
@@ -296,11 +247,8 @@ def get_net(ctx):
 
 # ## 训练模型并调参
 # 
-# 在[过拟合](../chapter_supervised-learning/underfit-overfit.md)中我们讲过，过度依赖训练数据集的误差来推断测试数据集的误差容易导致过拟合。由于图像分类训练时间可能较长，为了方便，我们这里不再使用K折交叉验证，而是依赖验证集的结果来调参。
-# 
 # 我们定义模型训练函数。这里我们记录每个epoch的训练时间。这有助于我们比较不同模型设计的时间成本。
 
-# In[52]:
 
 
 import datetime
@@ -348,27 +296,25 @@ def train(net, train_data, valid_data, num_epochs, lr, wd, ctx, lr_period, lr_de
 # 
 # 我们将依据验证集的结果不断优化模型设计和调整参数。依据下面的参数设置，优化算法的学习率将在每80个epoch自乘0.1。
 
-# In[53]:
 
 
 ctx = utils.try_gpu()
-num_epochs = 100
+num_epochs = 120
 learning_rate = 0.05
 weight_decay = 5e-4
-lr_period = 50
+lr_period = 80
 lr_decay = 0.1
 
 net = get_net(ctx)
 net.hybridize()
-# train(net, train_data, valid_data, num_epochs, learning_rate, 
-#       weight_decay, ctx, lr_period, lr_decay)
+
 
 
 # ## 对测试集分类
 # 
 # 当得到一组满意的模型设计和参数后，我们使用全部训练数据集（含验证集）重新训练模型，并对测试集分类。
 
-# In[ ]:
+
 
 
 import numpy as np
@@ -392,23 +338,3 @@ df['label'] = df['label'].apply(lambda x: train_valid_ds.synsets[x])
 df.to_csv('submission.csv', index=False)
 
 print("Done. :)")
-
-
-# 上述代码执行完会生成一个`submission.csv`的文件用于在Kaggle上提交。这是Kaggle要求的提交格式。这时我们可以在Kaggle上把对测试集分类的结果提交并查看分类准确率。你需要登录Kaggle网站，打开[CIFAR-10原始图像分类问题](https://www.kaggle.com/c/cifar-10)，并点击下方右侧`Late Submission`按钮。
-# 
-# ![](../img/kaggle_submit3.png)
-# 
-# 
-# 请点击下方`Upload Submission File`选择需要提交的预测结果。然后点击下方的`Make Submission`按钮就可以查看结果啦！
-# 
-# ![](../img/kaggle_submit4.png)
-# 
-# 
-# 
-# ## 作业（[汇报作业和查看其他小伙伴作业](https://discuss.gluon.ai/t/topic/1545/)）：
-# 
-# * 使用Kaggle完整CIFAR-10数据集，把batch_size和num_epochs分别改为128和100，可以在Kaggle上拿到什么样的准确率和名次？
-# * 如果不使用增强数据的方法能拿到什么样的准确率？
-# * 你还有什么其他办法可以继续改进模型和参数？小伙伴们都期待你的分享。
-# 
-# **吐槽和讨论欢迎点**[这里](https://discuss.gluon.ai/t/topic/1545/)
